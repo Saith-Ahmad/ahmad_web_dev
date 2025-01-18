e1e1bb4ac3cc1a1e13daa026314cee9809f3328a
saith ahmad <ahmadwebcrafts@gmail.com>
2025-01-18 16:40:58 +0500

    og preview image testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           property="og:description"
           content="A Fullstack developer solution to your customized web requirements"
         />
-        <meta property="og:image" content="https://ahmad-web-dev.vercel.app/preview.png" />
+        <meta property="og:image" content="https://ahmad-web-dev.vercel.app/preview.PNG" />
         <meta property="og:url" content="https://ahmad-web-dev.vercel.app" />
         <meta property="og:type" content="website" />
 
@@ -41,7 +41,7 @@ export default function RootLayout({
           name="twitter:description"
           content="A Fullstack developer solution to your customized web requirements"
         />
-        <meta name="twitter:image" content="https://ahmad-web-dev.vercel.app/preview.png" />
+        <meta name="twitter:image" content="https://ahmad-web-dev.vercel.app/preview.PNG" />
       </Head>
 
       <body className={`${FiraCode.className} antialiased`}>
