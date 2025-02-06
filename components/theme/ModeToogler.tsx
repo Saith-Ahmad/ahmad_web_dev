4c7039abdb05b37a87e2955e0cedb9be33f22709
Muhammad Ahmad <ahmadwebcrafts@gmail.com>
2025-02-07 01:36:59 +0500

    add ripple effect to the mode toogler

--- components/theme/ModeToogler.tsx
+++ components/theme/ModeToogler.tsx
@@ -11,7 +11,8 @@ export function ModeToggle() {
   const { setTheme, theme } = useTheme()
 
   return (
-    <Button variant="outline" size={'icon'}  onClick={() => setTheme(theme === 'light' ? "dark" : "light")} className="bg-text_purple">
+    <Button variant="outline" size={'icon'}  onClick={() => setTheme(theme === 'light' ? "dark" : "light")} className="bg-text_purple relative">
+      <span className="absolute inset-0 scale-110 -z-[5] rounded-md opacity-40 wave"></span>
       {theme === 'light' ? <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" fill="#fff" color="#fff"/>
         :
         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" fill="#fff"/>
